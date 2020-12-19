06621e72004d51599c0f50b82b99a8e26f3f3361
Alexander Reynoso Я <61547337+EAR1610@users.noreply.github.com>
2020-12-19 08:59:57 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function(){
+    newFunction();
+})
+
+function newFunction() {
+    $('.galeria .contenedor-imagen').on('click', function () {
+        $('#modal').modal;
+        var ruta_imagen = ($(this).find('img').attr('src'));
+        $('#imagen-modal').attr('src', ruta_imagen);
+    });
+
+    $('#modal').on('click', function () {
+        $('#modal').modal('hide');
+    });
+}
